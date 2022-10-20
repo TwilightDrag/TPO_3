f4a7e86f57729c288876a2b6b21b76ee27045212
lond512 <57224850+lond512@users.noreply.github.com>
2022-10-20 21:16:34 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -129,7 +129,7 @@ def vopros(raynd):
   bred1=rand_otveti.get(raynd*3)
   bred2=rand_otveti.get(raynd*3-1)
   bred3=rand_otveti.get(raynd*3-2)
-  return voprosik, otvetic
+  return voprosik, otvetic, bank, bred1, bred2, bred3
 
 
 def main():
